080076cf946470f9b155cd52d228eb70adfc7e4c
Pascal Salvador <smv7.github@gmail.com>
2022-03-29 13:56:58 -0300

    Adding setup files and basic code structure of template.py file

---
+++ template.py
@@ -0,0 +1,15 @@
+'''2022/03/29 by PianisT
+This terminal application creates text files bases on templates.'''
+
+
+from datetime import date
+
+import click
+
+
+def template():
+    pass
+
+
+if __name__ == '__main__':
+    template()
