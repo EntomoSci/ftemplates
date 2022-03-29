88018dfb5022f349bede21796a4d512fc2c14bb6
Pascal Salvador <smv7.github@gmail.com>
2022-03-29 18:43:01 -0300

    Fixing FileNotFoundErrors using path objects with os.path to support '~' chars for home directory

--- template.py
+++ template.py
@@ -2,59 +2,70 @@
 This terminal application creates text files bases on templates.'''
 
 
-from datetime import date
-from email.policy import default
+import os
+from datetime import datetime
 
 import click
 
 
+# Constructing a path object that suports the "~" character to refer home directory.
+path = os.path.expanduser('~/.TextFileTemplates/')
+
+
 @click.group()
 def template():
-    pass
+    # Creating required hidden directory "TextFileTemplates" in home directory if is not created yet. 
+    if os.path.isdir(path):
+        pass
+    else:
+        click.echo('Creating required directory "TextFileTemplates" in home directory...')
+        os.mkdir(path)
 
 
-@click.option('--new_template', is_flag=True, help='Create a new template')
-@click.option('--new_file', is_flag=True, help='Create file using an existing template')
-@click.argument('arg1', required=True)
-@click.argument('arg2', required=True)
-def create(new_template, new_file, arg1, arg2):
-    '''Creates a new template or file using an existing template.'''
+@template.command()
+@click.option('--new_template', is_flag=True, default=None, help='Create a new template')
+@click.option('--new_file', is_flag=True, default=True, help='Create file using an existing template')
+@click.argument('file_to_use', required=True)
+@click.argument('file_to_create', required=True)
+def create(new_template, new_file, file_to_use, file_to_create):
+    '''Creates a new template or a new file using an existing template.'''
 
     # If the "--new_template" flag is used, the program checks if already exist a file
     # with the name given by the argument. If not exist, creates the new template in the
     # "templates" folder of this project.
-    if new_template is not None:
-        file_name = arg1 #input('File path to use as template: ')
+    if new_template:
+        # Checks if the file to use for create the new template exist.
         try:
-            with open(file_name) as f:
+            with open(file_to_use, 'r') as f:
                 template = f.read()
         except FileNotFoundError:
-            click.echo('The file do not exist.')
+            click.echo(f'The file to use "{file_to_use}" do not exist.')
             return None
+
+        # Checks if the template file to create already exist.
+        try: 
+            with open(f'{path}{file_to_create}') as f:
+                pass 
+        except FileNotFoundError:
+            click.echo(f'Creating new template "{file_to_create}"...')
+            with open(f'{path}{file_to_create}', 'x') as template_to_create:
+                template_to_create.write(template) 
+            click.echo(f'"{file_to_create}" successfully created!')
+            click.echo(f'"{file_to_create}" is now available to use as a template for new text files.')
         else:
-            while True:
-                template_name = arg2 #input('Template name: ')
-                try: 
-                    with open(f'./templates/{template_name}') as f:
-                        pass 
-                except FileNotFoundError:
-                    click.echo(f'Creating new template "{template_name}"...')
-                    with open(f'./templates/{template_name}', 'w') as f:
-                        f.write(template) 
-                    click.echo(f'"{template_name}" successfully created!')
-                    click.echo(f'"{template_name}" is now available to use as a template for new text files.')
-                else:
-                    click.echo(f'A file with name "{template_name}" already exist.')
-                    return None
+            click.echo(f'A file with name "{file_to_create}" already exist.')
+            return None
         finally:
-            click.echo('All executed.\n')
+            click.echo('All executed.')
                     
     # if the "--new_file" flag is used, the program creates a new file in the path given by
     # the argument passed (default is current directory) using a template from the "templates"
     # folder as second argument.
-    if new_file is not None:
+    elif new_file is not None:
         pass
 
 
 if __name__ == '__main__':
+
+    
     template()
