85f85b738f9a89e60bff427c526e758538cc5f00
smv7 <smv7.github@gmail.com>
2023-01-22 19:01:08 -0300

    Clean proj struct & add poetry build fixes
    The project updload/download was tested in test.pypi with poetry
    successfully. Additional polishes were added to the proj structure.

--- templates/cli.py
+++ templates/cli.py
@@ -11,7 +11,7 @@ from pathlib import Path
 import click
 
 
-TEMP_DIR_PATH = Path(__file__).parents[1] / "available_templates"
+TEMP_DIR_PATH = Path(__file__).parent / "available_templates"
 
 
 @click.group()
@@ -88,9 +88,7 @@ def list():
     if len(templates) > 0:
         click.echo("TEMPLATES:\n")
         for template in templates:
-            # NOTE: Extra resources are required to be packaged as packages with Poetry in pyproject.toml's [packages].
-            if template.name != "__init__.py":
-                click.echo(f"\t- {template.name}")
+            click.echo(f"\t- {template.name}")
         click.echo('')
     else:
         click.echo("You've no templates yet.\n")
